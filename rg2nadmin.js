3f4477d546102a052366a7401237d75ce86a7720
Jake-Jacob <64289295+Jake-Jacob@users.noreply.github.com>
2021-05-12 09:48:06 -0400

    Update rg2nadmin.js

--- rg2nadmin.js
+++ rg2nadmin.js
@@ -1,1 +1,58 @@
+// Default Variables
+const version = "V1.0"
+const port = Game.port
+const serverversion = Game.version
 
+// Config Loader
+const config = Game.serverSettings.rg2nAdmin
+
+// Config
+const staff = config.staff || [],
+      restricted = config.restricted || [];
+
+// Functions
+function checkStaff(player) {
+    if (staff.includes(player.userId)) return true
+}
+function checkRestricted(player) {
+    if (restricted.includes(player.userId)) return true
+}
+
+// Code
+Game.on("playerJoin", player => {
+    player.message("This server is equipped with RG2N Admin " + version)
+    if (checkRestricted(player) == true) {
+        player.kick("You are restricted from joining this game!")
+    }
+})
+
+Game.command("isstaff", player => {
+    if (checkStaff(player) == true) {
+        player.message("You are staff!")
+    } else {
+        player.message("You are not staff!")
+    }
+})
+
+Game.command("serverinfo", (player, Game) => {
+    player.message(`\\c6[SERVER]: \\c0Port: ` + String(port) + ' Version: ' + serverversion)
+})
+
+Game.command("kick", (caller, args) => {
+    if (checkStaff(caller) !== true) return
+    for (let player of Game.players) {
+        if (player.username.startsWith(args)) {
+            return player.kick("You have been removed from the server.")
+        }
+    }
+})
+
+Game.command("ban", (caller, args) => {
+    if (checkStaff(caller) !== true) return
+    for (let player of Game.players) {
+        if (player.username.startsWith(args)) {
+            restricted.push(player.userId)
+            return player.kick("You have been banned from the server.")
+        }
+    }
+})
